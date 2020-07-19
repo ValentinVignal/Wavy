0485e6667cbdf3ca69a12cdf75aa8aa9ca69c043
ValentinVignal <valentin.vignal.dev@outlook.fr>
2020-07-19 14:59:27 +0800

    Move to linux

--- main.cpp
+++ main.cpp
@@ -1,5 +1,16 @@
 #include <iostream>
+#include <vector>
+#include <string>
 
-int main() {
-    std::cout << "Hello World" << std::endl;
+using namespace std;
+
+int main()
+{
+    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
+    
+    for (const string& word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
 }
